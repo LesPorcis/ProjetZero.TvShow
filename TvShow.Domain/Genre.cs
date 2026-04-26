298fc80a7b3de7d7a25ae61b67c4913234a9ffa7
Clément Farina <clem-du-45@live.fr>
2026-04-26 16:51:33 +0200

    First commit !

---
+++ TvShow.Domain/Genre.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TvShow.Domain;
+
+public class Genre
+{
+    [Required]
+    public int Id { get; set; }
+    
+    // Forcer l'unicité du nom
+    public string Name { get; set; } = string.Empty;
+    
+    public string Description { get; set; } = string.Empty;
+    
+    public IReadOnlyCollection<TvShow> TvShow { get; set; } = new  List<TvShow>();
+}
---
+++ TvShow.Domain/Star.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TvShow.Domain;
+
+public class Star
+{
+    [Required]
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    
+    public IReadOnlyCollection<TvShow> TvShow { get; set; } = new  List<TvShow>();
+}
