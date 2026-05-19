995e3f9c4dcf7659458a2250f4d9443fb50a3b8e
Clément Farina <clem-du-45@live.fr>
2026-05-19 18:25:01 +0200

    ✨ Test github notif

--- TvShow.Infrastructure/DependencyInjection.cs
+++ TvShow.Infrastructure/DependencyInjection.cs
@@ -4,6 +4,9 @@ using TvShow.Infrastructure.Repositories;
 
 namespace TvShow.Infrastructure;
 
+/// <summary>
+/// Méthode d'extension pour ajouter les services d'infrastructure
+/// </summary>
 public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
